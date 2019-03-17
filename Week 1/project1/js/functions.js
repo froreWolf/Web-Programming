f6d34ae8a35d19201b3aa975c65bd29719ee28b1
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2019-03-17 18:41:05 -0400

    Worked on Project1

--- Week 1/project1/js/functions.js
+++ Week 1/project1/js/functions.js
@@ -1,4 +1,42 @@
 /* define functions here */
+function calculateTotal(quantity, price){
+  return quantity * price;
+}
 
+function outputCartRow(file, title, quantity, price, total) {
+  price = "$" + price.toFixed(2);
+  total = "$" + total.toFixed(2);
+  document.write('<tr>');
+  document.write('<td><img src="images/' + file + '"</td>');
+  document.write('<td>' + title + '</td>');
+  document.write('<td>' + quantity + '</td>');
+  document.write('<td>' + price + '</td>');
+  document.write('<td>' + total + '</td>');
+  document.write('</tr>')
+}
 
-        
+function getSubtotal(){
+  var subtotal = 0;
+  for (i = 0; i < prices.length; i++){
+     subtotal += calculateTotal(quantities[i], prices[i]);
+  }
+
+  return subtotal
+}
+
+function getTax(){
+  return 0.10 * getSubtotal();
+}
+
+function getshipping(){
+  if (getSubtotal() > 1000){
+    return 0
+  }
+  else {
+    return 40
+  }
+}
+
+function getGrandTotal(){
+  return (getSubtotal() + getTax() + getshipping())
+}
