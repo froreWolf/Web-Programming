c5157edb1269f52db6e27661543ea2e827bb08e3
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2019-03-24 17:49:14 -0400

    failed at project 1

--- Week 2/project1/js/chapter09-project01.js
+++ Week 2/project1/js/chapter09-project01.js
@@ -1,2 +1,14 @@
-
-/* add code here  */
+window.addEventListener('load', function(){
+//get a list of the elements that have the class hilightable
+  var hilightable = document.querySelectorAll('input.hilightable');
+  for(var i = 0; i < hilightable.length; i++){
+    //add the focus events
+    hilightable[i].addEventListener('focus', function(){
+    hilightable[i].classList.add('highlight');
+    });
+    //add blur events
+    hilightable[i].addEventListener('blur', function(){
+    hilightable[i].classList.remove('highlight');
+    });
+  };
+});
--- Week 2/project2/js/chapter09-project02.js
+++ Week 2/project2/js/chapter09-project02.js
@@ -1,1 +1,10 @@
-/* add code here */
+document.getElementById("thumbnails").addEventListener('click', function(e){
+//e.target is the clicked element
+var source = e.target
+alert(source.attributes);
+});
+
+var items = document.getElementByTagName("img");
+for(var i = 0; i< items.length; i++){
+  items[i].textContent = 'src="images/small/5855774224.jpg" title="Battle" alt="Battle"
+}
